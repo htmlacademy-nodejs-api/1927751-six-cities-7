cf6b26abba001b24b0f9b6edfb0c07db9a639a03
Siarhei Yarashuk <siarhei.yarashuk@softswiss.com>
2024-06-17 22:22:47 +0200

    feat: include auth module

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -23,7 +23,9 @@ export class RestApplication {
     @inject(Component.UserController)
     private readonly userController: IController,
     @inject(Component.CommentController)
-    private readonly commentController: IController
+    private readonly commentController: IController,
+    @inject(Component.AuthExceptionFilter)
+    private readonly authExceptionFilter: IExceptionFilter
   ) {
     this.server = express();
   }
@@ -60,6 +62,9 @@ export class RestApplication {
   }
 
   private async _initExceptionFilters() {
+    this.server.use(
+      this.authExceptionFilter.catch.bind(this.authExceptionFilter)
+    );
     this.server.use(
       this.appExceptionFilter.catch.bind(this.appExceptionFilter)
     );
