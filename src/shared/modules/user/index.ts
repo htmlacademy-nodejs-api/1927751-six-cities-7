72419eeae0d686c6c851ab2ada0c486d22be353b
Siarhei Yarashuk <siarhei.yarashuk@softswiss.com>
2024-06-17 22:10:47 +0200

    feat: auth module

--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,6 +1,8 @@
 export { UserEntity, UserModel } from './user.entity.js';
 
 export { CreateUserDto } from './dto/create-user.dto.js';
+export { LoginUserDto } from './dto/login-user.dto.js';
+
 export { DefaultUserService } from './default-user.service.js';
 export { createUserContainer } from './user.container.js';
 export { IUserService } from './user-service.interface.js';
